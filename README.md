042d9c41ecb03cbf8390014e05252c3a7915137d
Alberto Gimeno <gimenete@gmail.com>
2017-11-22 17:54:49 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,64 @@
+# Browse
+
+Command line utility to open links found in package.json
+
+## Usage
+
+Put in your `package.json` file a bunch of links with any tree strcuture you prefer. The leaves must be URLs.
+
+```
+"browse": {
+  "website": "https://example.com/",
+  "sentry": "https://sentry.example.com/example/project-name/",
+  "development": {
+    "server": {
+      "api": "https://api-dev.example.com/",
+      "frontend": "https://dev-dev.example.com/"
+    },
+    "sumologic": {
+      "api": "https://www.sumologic.com/whatever",
+      "cms": "https://www.sumologic.com/whatever",
+      "frontend": "https://www.sumologic.com/whatever"
+    }
+  }
+}
+```
+
+Now you have a few options:
+
+### Using npx
+
+Just run this in the directory where your `package.json` file is:
+
+```
+npx github:clevertech/browse [path]
+```
+
+### Installing locally
+
+Install by using either:
+
+```
+npm install @clevertech.biz/browse -D
+yarn install @clevertech.biz/browse -D
+```
+
+Add a local script in your `package.json`:
+
+```
+"scripts": {
+  "browse": "browse"
+}
+```
+
+Then run:
+
+```
+npm/yarn run browse [path]
+```
+
+## Autocomplete
+
+`open` works in two different ways. If you don't provide any command line argument it will print all available links and an autocomplete interface.
+
+If you provide the path, it will open the link right away.
---
+++ index.js
@@ -0,0 +1,78 @@
+#!/usr/bin/env node
+
+const path = require('path')
+const opn = require('opn')
+
+const readPackage = () => {
+  try {
+    return require(path.join(process.cwd(), 'package.json'))
+  } catch (err) {
+    throw new Error('Could not read package.json in the current directory')
+  }
+}
+
+const errorHandler = err => {
+  console.error(err)
+  process.exit(1)
+}
+
+const run = () => {
+  const packageJSON = readPackage()
+
+  const findLinks = (path, obj, hash) => {
+    if (Object.prototype.toString.call(obj) === '[object Object]') {
+      Object.keys(obj).forEach(key => {
+        const value = obj[key]
+        findLinks(path.concat(key), value, hash)
+      })
+    } else if (typeof obj === 'string') {
+      hash[path.join(' ')] = obj
+    }
+    return hash
+  }
+
+  const links = findLinks([], packageJSON.browse, {})
+
+  const open = input => {
+    const url = links[input]
+    if (!url) {
+      console.error(`Could not find a link for "${input}"`)
+      process.exit(1)
+    }
+    console.log(`Opening ${url} in your browser...`)
+    return opn(url, { wait: false })
+      .catch(errorHandler)
+  }
+
+  if (process.argv.length === 2) {
+    const term = require('terminal-kit').terminal
+    term('Available links to open\n')
+    Object.keys(links).forEach(path => {
+      term.cyan(path + '\n')
+      term.green(`    ${links[path]}\n`)
+    })
+    term('Please enter the path to the link you want to open (autocomplete with tabulator): ')
+    const autoComplete = Object.keys(links)
+
+    term.inputField({ autoComplete, autoCompleteMenu: true }, (err, input) => {
+      console.log()
+      if (err) {
+        console.error(err)
+        process.exit(1)
+      }
+      const normalized = input.trim().split(/\W+/g).join(' ')
+      open(normalized).then(() => process.exit(0))
+    })
+  } else {
+    const input = process.argv.slice(2).join(' ')
+    open(input)
+  }
+}
+
+try {
+  run()
+} catch (err) {
+  console.error(err.message || String(err))
+  process.exit(1)
+}
+
